9b7954a656d47a3bed921d804b372f9214e1d255
ritaborlaug <rita.borlaug@gmail.com>
2023-10-13 10:46:12 +0200

    Initial Commit

---
+++ MyHealthApp/MyHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthAppApp.swift
+//  MyHealthApp
+//
+//  Created by Rita Borlaug on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
